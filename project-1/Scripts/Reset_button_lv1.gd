157afaa5b530bece4077f7f3504d5e3a9e5f7471
Chuan <cchua14@eq.edu.au>
2025-09-28 08:24:17 +1000

    Revert "Update2"
    This reverts commit bac465a19995f171f845f9d5550957bd90fb985c, reversing
    changes made to 0d092f0d8641cf3015e9e6a7c5d494082a9f926e.

--- project-1/Scripts/Reset_button_lv1.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-
-func _on_pressed() -> void:
-	Lv1Main.level1_coin = 0
-	MainMenu.death += 1
-	get_tree().reload_current_scene()
--- project-1/Scripts/coinlv1label.gd
+++
@@ -1,4 +0,0 @@
-extends Label
-
-func _process(delta: float) -> void:
-	$".".text = 'Coin:' + str(Lv1Main.level1_coin)
