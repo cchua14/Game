553e3c124d25a9279ad425769c7ebb2c2543c278
Chuan <cchua14@eq.edu.au>
2025-09-28 15:07:34 +1000

    fix gambler

--- project-1/Scripts/coin gameblelv1.gd
+++ project-1/Scripts/coin gameblelv1.gd
@@ -3,11 +3,11 @@ extends Button
 
 
 func _on_pressed() -> void:
-	if Main2.level2_coin >= 2:
-		Main2.level2_coin -= 2
+	if Lv1Main.level1_coin >= 2:
+		Lv1Main.level1_coin -= 2
 		var w_or_l = ['win','lose'].pick_random()
 		if w_or_l == 'win':
-			Main2.level2_coin += 4
+			Lv1Main.level1_coin += 4
 		else:
 			pass
 	else:
