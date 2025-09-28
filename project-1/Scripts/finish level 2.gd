ca5cd953edf40f01ddd64b3d4fd76523567f7e88
Chuan <cchua14@eq.edu.au>
2025-09-28 16:09:16 +1000

    Update finish level 2.gd

--- project-1/Scripts/finish level 2.gd
+++ project-1/Scripts/finish level 2.gd
@@ -4,4 +4,4 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if Main2.level2_coin >= 9:
 		Main2.level2_coin -= 9
-		get_tree().change_scene_to_file("res://Scenes/end_menu.tscn")
+		get_tree().change_scene_to_file('res://Scenes/level_3.tscn')
