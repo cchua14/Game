bc78728c3f6a0c0410f4f576d6695065644fcd43
Chuan <cchua14@eq.edu.au>
2025-09-28 14:52:24 +1000

    update lv 5

--- project-1/Scripts/clicker/l5char.gd
+++ project-1/Scripts/clicker/l5char.gd
@@ -3,17 +3,21 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+
+
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
+		$AnimatedSprite2D.flip_h = direction < 0
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	var up_down := Input.get_axis('ui_up','ui_down')
-	if up_down:
-		velocity.y = up_down * SPEED
+	
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	if direction_y:
+		velocity.y = direction_y * SPEED
 	else:
-		velocity.y = move_toward(velocity.y, 0 , SPEED)
-		
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
 	move_and_slide()
